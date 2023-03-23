86a86247affc8f941ff9f52d3ec34197d1fab39e
Vicente Morales <vicente.morales@HPPFS.local>
2023-03-23 14:50:15 -0700

    improved jquery to create elements. updated the fetch PATCH

--- public/admin.js
+++ public/admin.js
@@ -1,11 +1,49 @@
 async function mainAdmin() {
     let response = await fetch('http://localhost:3001/listBooks');
     let books = await response.json();
+    //for each object in books (fetched array) the content book is used to run adminList()
     books.forEach(adminList);
     console.log(books)
   }
   
   function adminList(book){
+
+    //selects the query with the ID of 'adminish'
+    let root = document.querySelector('#adminish')
+
+    // sets variable li to create element <li></li>
+    // variables text content is the book title from the data fetched in mainAdmin()
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    //creates a variable with a value of a '<input></input>' prefilled with the value of book.quantity
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    //creates a save button (text content of sa)
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    //adds an event listener to the button. 
+    //When clicked, the function will PATCH the 
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+
+    li.append(quantityInput, saveButton)
+
+    root.append(li)
+
+    /*
     let adminContainer = document.getElementById('adminish');
     $(adminContainer).append(`
       <li>${book.title}</li>
@@ -29,7 +67,7 @@ async function mainAdmin() {
     })
   
     
-  });
+  })*/
 }
   mainAdmin();
   
